54250750891b7049bcd26ea30eb766fde293400d
elysia <3302843873@qq.com>
2025-12-14 17:48:57 +0800

    added environment variables about the xdg session types

--- home/niri.nix
+++ home/niri.nix
@@ -75,5 +75,9 @@
       # TODO: make the path to wallpaper a nix path (with ${../wallpaper} or something)
       { sh = "${pkgs.linux-wallpaperengine}/bin/linux-wallpaperengine ~/nix/wallpaper --screen-root DP-3 --silent --no-audio-processing --no-fullscreen-pause"; }
     ];
+
+    environment = {
+      XDG_SESSION_TYPE = "wayland";
+    };
   };
 }
