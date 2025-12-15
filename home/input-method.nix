96047b9717381e49b85cb8c3fa61fa3399f6fd5a
elysia <3302843873@qq.com>
2025-12-15 21:04:35 +0800

    [Generation 94] fixed input method

--- home/input-method.nix
+++ home/input-method.nix
@@ -4,9 +4,26 @@
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
-    fcitx5.addons = with pkgs; [
-      fcitx5-gtk
-      fcitx5-chinese-addons
-    ];
+    fcitx5 = {
+      addons = with pkgs; [
+        fcitx5-gtk
+        fcitx5-chinese-addons
+      ];
+
+      settings.inputMethod = {
+        GroupOrder."0" = "Default";
+	"Groups/0" = {
+          Name = "Default";
+	  "Default Layout" = "us";
+	  DefaultIM = "pinyin";
+	};
+	"Groups/0/Items/0".Name = "keyboard-us";
+	"Groups/0/Items/1".Name = "pinyin";
+      };
+    };
   };
+
+  programs.niri.settings.spawn-at-startup = [
+    { sh = "fcitx5 -d -r"; }
+  ];
 }
