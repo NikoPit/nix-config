37b8fc6ed9781f039bebd276f866170fa0f48b17
elysia <3302843873@qq.com>
2025-12-15 18:58:05 +0800

    [Generation 91] tried to make xdg desktop portal work x2

--- os/desktop-portal.nix
+++ os/desktop-portal.nix
@@ -1,17 +1,8 @@
 { pkgs, ... }:
 
 {
-  xdg.portal = {
-    enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-gtk
-      xdg-desktop-portal-gnome
-    ];
-
-    config = {
-      common = { default = [ "gtk" ]; };
-    };
-
-    configPackages = [ pkgs.xdg-desktop-portal-gtk ];
-  };
+  environment.systemPackages = with pkgs; [
+    xdg-desktop-portal-gtk
+    xdg-desktop-portal-gnome
+  ];
 }
