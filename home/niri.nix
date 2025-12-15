1d7606882bae0377b5d4470ae5a717db541bc87a
elysia <3302843873@qq.com>
2025-12-15 18:21:50 +0800

    [Generation 88] moved waybar autostart to waybar.nix

--- home/niri.nix
+++ home/niri.nix
@@ -71,7 +71,6 @@
     };
 
     spawn-at-startup = [
-      { sh = "waybar"; } 
       # TODO: make the path to wallpaper a nix path (with ${../wallpaper} or something)
       { sh = "${pkgs.linux-wallpaperengine}/bin/linux-wallpaperengine ~/nix/wallpaper --screen-root DP-3 --silent --no-audio-processing --no-fullscreen-pause"; }
     ];
--- home/waybar.nix
+++ home/waybar.nix
@@ -4,5 +4,8 @@
 
     settings = {};
   };
-
+  
+  programs.niri.settings.spawn-at-startup = [
+    { sh = "waybar"; }
+  ];
 }
