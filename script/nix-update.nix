52f8298909d7900b4a71f46b3d176c62154ffd9c
elysia <3302843873@qq.com>
2025-12-15 15:50:30 +0800

    changed nix-update script to track nix generation

--- script/nix-update.nix
+++ script/nix-update.nix
@@ -2,8 +2,12 @@
 
 
 pkgs.writeShellScriptBin "nix-update" ''
+  nix_generation=$(cat ./.nix-generation)
+  new_nix_generation=$(($nix_generation+1))
+  echo $new_nix_generation >./.nix-generation
+
   git add ./*
-  git commit -m "$1"
+  git commit -m "[Generation "$new_nix_generation"] $1"
   sudo nixos-rebuild switch --flake ./ 
 ''
 
