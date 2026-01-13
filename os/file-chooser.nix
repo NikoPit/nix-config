384f37148ee353a3b1be9a4208e517c9814a1581
elysia <3302843873@qq.com>
2026-01-13 17:31:06 +0800

    [Generation 183] use yazi as file chooser

---
+++ os/file-chooser.nix
@@ -0,0 +1,24 @@
+{
+  xdg = {
+    portal = {
+      extraPortals = [pkgs.xdg-desktop-portal-termfilechooser];
+
+      config = {
+        common."org.freedesktop.impl.portal.FileChooser" = "termfilechooser";
+        niri."org.freedesktop.impl.portal.FileChooser" = "termfilechooser";
+      };
+    };
+  };
+
+  programs.firefox.preferences."widget.use-xdg-desktop-portal.file-picker" = 1;
+
+  environment.variables.TERMCMD = "${lib.getExe pkgs.ghostty} --title='termfilechooser' -e";
+
+  hjem.users.elysia.xdg.config.files."xdg-desktop-portal-termfilechooser/config" = {
+    text = ''
+      [filechooser]
+      cmd=${pkgs.xdg-desktop-portal-termfilechooser}/share/xdg-desktop-portal-termfilechooser/yazi-wrapper.sh
+    '';
+  };
+
+}
