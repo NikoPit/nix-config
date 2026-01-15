6c4bc7395486af97020017f8af7212b78161d4ec
elysia <3302843873@qq.com>
2026-01-15 21:53:35 +0800

    [Generation 224] dunst

--- home/dunst.nix
+++ home/dunst.nix
@@ -9,10 +9,12 @@
 	icon_corner_radius = 10;
 	corner_radius = 10;
 	offset = "(0, 100)";
-	width = "(0, 500)";
+	width = "(200, 400)";
 	gap_size = 10;
 	corners = "left";
 	frame_width = 0;
+
+	bgcolor = "#2596be80";
       };
     };
   };
