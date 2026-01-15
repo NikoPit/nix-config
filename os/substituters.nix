b63fcd46f81ec82e9f0894709ef5107e0f9bbea3
elysia <3302843873@qq.com>
2026-01-15 14:25:37 +0800

    [Generation 214] added a new substituter

--- os/substituters.nix
+++ os/substituters.nix
@@ -1,9 +1,8 @@
 {
-    nix.settings = {
-        substituters = [
-	    "https://nix-citizen.cachix.org"
-            "https://mirrors.ustc.edu.cn/nix-channels/store" 
-	];
-        trusted-public-keys = ["nix-citizen.cachix.org-1:lPMkWc2X8XD4/7YPEEwXKKBg+SVbYTVrAaLA2wQTKCo="];
-    };
+  nix.settings = {
+    substituters = [
+      "https://mirrors.ustc.edu.cn/nix-channels/store" 
+      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+    ];
+  };
 }
