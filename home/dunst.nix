11c2d4fadea52c9295ceebb6476dee05e48b91fa
elysia <3302843873@qq.com>
2026-01-15 20:18:37 +0800

    [Generation 221] dunst

--- home/dunst.nix
+++ home/dunst.nix
@@ -4,7 +4,13 @@
     settings = {
       global = {
         origin = "bottom-right";
-	font = "JetBrains Mono 12";
+	font = "JetBrains Mono 11";
+	progress_bar = true;
+	icon_corner_radius = 10;
+	corner_radius = 10;
+	offset = "(0, 100)";
+	width = "(0, 500)";
+	gap_size = 10;
       };
     };
   };
