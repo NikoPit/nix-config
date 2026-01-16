d66a150fdfe05791d817ee1b3149a75d6d8aa682
elysia <3302843873@qq.com>
2026-01-16 10:08:19 +0800

    [Generation 225] wallpaper

--- home/wallpaper.nix
+++ home/wallpaper.nix
@@ -2,5 +2,10 @@
   services.linux-wallpaperengine = {
     enable = true;
     assetsPath = ../wallpaper;
+
+    wallpapers = [ {
+      monitor = "DP-3";
+      wallpaperId = "1";
+    } ];
   };
 }
