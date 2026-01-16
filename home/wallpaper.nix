0decfd1d95e0803e8ddb4196a65c90a1e2aa6b40
elysia <3302843873@qq.com>
2026-01-16 14:16:05 +0800

    [Generation 230] wallpaper

--- home/wallpaper.nix
+++ home/wallpaper.nix
@@ -2,11 +2,13 @@
   services.linux-wallpaperengine = {
     enable = true;
     assetsPath = ../wallpaper;
+    clamping = "clamp";
 
     wallpapers = [ {
       monitor = "DP-3";
       wallpaperId = "3203778110";
       scaling = "default";
+      fps = 60;
       audio = {
 #        silent = true;
 #	processing = false;
