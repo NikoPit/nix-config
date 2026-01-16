538edbc7aa32d2e75305943bd57194f44d688b4c
elysia <3302843873@qq.com>
2026-01-16 15:12:29 +0800

    [Generation 234] wallpaper

--- home/wallpaper.nix
+++ home/wallpaper.nix
@@ -1,15 +1,20 @@
+{ pkgs, ... }:
+
 {
   services.linux-wallpaperengine = {
     enable = true;
-    #assetsPath = ../wallpaper;
-
-    wallpapers = [ {
-      monitor = "DP-3";
-      wallpaperId = "3203778110";
-      audio = {
-        #silent = true;
-	#processing = false;
-      };
-    } ];
+    
+    # TODO: WIP
+    #wallpapers = [ {
+    #  monitor = "DP-3";
+    #  backgroundId = "3203778110";
+    #  audio = {
+    #    silent = true;
+    #	processing = false;
+    #  };
+    #} ];
   };
+
+  programs.niri.settings.spawn-at-startup = [{ sh = "${pkgs.linux-wallpaperengine}/bin/linux-wallpaperengine ../wallpaper/3203778110 
+    --screen-root DP-3 --silent --no-audio-processing --no-fullscreen-pause"; }];
 }
