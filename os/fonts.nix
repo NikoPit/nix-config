fd820942df2ed3f30af693f000cb78d8b0819028
elysia <3302843873@qq.com>
2026-01-28 10:41:08 +0800

    changed default fonts for OS to maple mono

--- os/fonts.nix
+++ os/fonts.nix
@@ -5,5 +5,13 @@
     packages = with pkgs; [
       maple-mono.NF-CN-unhinted
     ];
+
+    fontconfig = {
+      defaultFonts = {
+        serif = [ "Maple Mono NF CN" ];
+        sansSerif = [ "Maple Mono NF CN" ];
+        monospace = [ "Maple Mono NF CN" ];
+      };
+    };
   };
 }
