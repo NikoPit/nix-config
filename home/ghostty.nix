54d8d87be1155ec2d0b559afd09c28e371300b62
elysia <3302843873@qq.com>
2026-01-28 10:21:04 +0800

    updated ghostty font config to NFCN

--- home/ghostty.nix
+++ home/ghostty.nix
@@ -3,7 +3,7 @@
     enable = true;
 
     settings = {
-        font-family = "Maple Mono CN";
+        font-family = "Maple Mono NF CN";
 	font-size = 12;
     	window-decoration = "none";
 	background-opacity = 0.7;
