c178a226e95dd66ba95a40e31448cf20716a2a16
elysia <3302843873@qq.com>
2026-02-10 10:22:08 +0800

    avante

--- nixvim/avante.nix
+++ nixvim/avante.nix
@@ -3,6 +3,13 @@
     enable = true;
     settings = {
       provider = "deepseek";
+      providers = {
+        deepseek = {
+	  					endpoint = "https://api.deepseek.com";
+					model = "deepseek-coder";
+					max_tokens = 8192;
+	};
+      };
     };
   };
 }
