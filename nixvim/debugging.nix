ef9dbe5a235f68a08fc8f3e09366764f2fe20d97
elysia <3302843873@qq.com>
2026-02-13 17:49:30 +0800

    a

--- nixvim/debugging.nix
+++ nixvim/debugging.nix
@@ -19,7 +19,7 @@
 dap.configurations.rust = {
     {
         name = "Attach to QEMU",
-        type = "gdb",
+        type = "rust-gdb",
         request = "attach",
         target = "localhost:1234", 
         program = function()
