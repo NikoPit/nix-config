edd98c05c3573a0ebc9261e037a83dbd484a8859
elysia <3302843873@qq.com>
2026-02-13 15:57:28 +0800

    a

--- nixvim/debugging.nix
+++ nixvim/debugging.nix
@@ -1,3 +1,5 @@
+{lib, ...}:
+
 {
   plugins = {
   dap-ui.enable = true;
@@ -11,4 +13,22 @@
     };
   };
   };
+
+  extraConfigLua = ''
+-- 配置调试目标
+dap.configurations.rust = {
+    {
+        name = "Attach to QEMU",
+        type = "gdb",
+        request = "attach",
+        target = "localhost:1234", -- 对应 qemu 的 -s 参数
+        -- 指向你编译出来的带调试信息的 ELF 文件
+        program = function()
+            return vim.fn.getcwd() .. "/target/x86_64-unknown-none/debug/your_kernel_name"
+        end,
+        cwd = "{workspaceRoot}",
+        stopOnEntry = false,
+    },
+}
+'';
 }
