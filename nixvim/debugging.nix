4f4286e2a6659969f571bffbe83d8c840bd38990
elysia <3302843873@qq.com>
2026-02-13 17:36:59 +0800

    a

--- nixvim/debugging.nix
+++ nixvim/debugging.nix
@@ -7,25 +7,21 @@
   dap = {
     enable = true;
 
+    executables.gdb = {
+      command = "rust-gdb";
+      args = ["-i" "dap"];
+    };
   };
   };
 
   extraConfigLua = ''
-local dap = require("dap")
-  dap.adapters.gdb = {
-    type = "executable",
-    command = "gdb-multiarch", -- 或者 x86_64-elf-gdb
-    args = { "--interpreter=dap", "--eval-command", "set pagination off" }
-}
-
 -- 配置调试目标
 dap.configurations.rust = {
     {
         name = "Attach to QEMU",
         type = "gdb",
         request = "attach",
-        target = "localhost:1234", -- 对应 qemu 的 -s 参数
-        -- 指向你编译出来的带调试信息的 ELF 文件
+        target = "localhost:1234", 
         program = function()
             return vim.fn.getcwd() .. "/target/x86_64-unknown-none/debug/your_kernel_name"
         end,
