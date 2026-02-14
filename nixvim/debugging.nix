0f6bdde4ae3688cdc4561a07a5473b08311bc741
elysia <3302843873@qq.com>
2026-02-14 11:49:23 +0800

    debugging stuff

--- nixvim/debugging.nix
+++ nixvim/debugging.nix
@@ -1,37 +1,61 @@
-{lib, ...}:
+{ lib, ... }:
 
 {
   plugins = {
-  dap-ui.enable = true;
-  dap-virtual-text.enable = true;
-  dap = {
-    enable = true;
+    dap-ui.enable = true;
+    dap-virtual-text.enable = true;
+    dap = {
+      enable = true;
 
-    adapters = {
-      executables.rust = {
-        command = "rust-gdb";
-	args = ["-i" "dap"];
+      adapters = {
+        executables.rust = {
+          command = "rust-gdb";
+          args = [
+            "-i"
+            "dap"
+          ];
+        };
+
+        configurations = # lua
+          {
+            __raw = ''
+                 dap.configurations.rust{
+                            name = "Attach to QEMU",
+                            type = "rust-gdb",
+                            request = "attach",
+                            target = "localhost:1234", 
+                            program = function()
+                                return vim.fn.getcwd() .. "/target/x86_64-elysia-os/debug/elysia-os"
+                            end,
+                            cwd = function()
+                    	    return vim.fn.getcwd()
+                    	end,    
+                            stopOnEntry = false,
+                        }
+
+              	'';
+          };
       };
     };
+
+    extraConfigLua = ''
+        local dap = require("dap")
+
+      dap.configurations.rust = {
+          {
+              name = "Attach to QEMU",
+              type = "rust-gdb",
+              request = "attach",
+              target = "localhost:1234", 
+              program = function()
+                  return vim.fn.getcwd() .. "/target/x86_64-elysia-os/debug/elysia-os"
+              end,
+              cwd = function()
+      	    return vim.fn.getcwd()
+      	end,    
+              stopOnEntry = false,
+          },
+      }
+    '';
   };
-
-  extraConfigLua = ''
-  local dap = require("dap")
-
-dap.configurations.rust = {
-    {
-        name = "Attach to QEMU",
-        type = "rust-gdb",
-        request = "attach",
-        target = "localhost:1234", 
-        program = function()
-            return vim.fn.getcwd() .. "/target/x86_64-elysia-os/debug/elysia-os"
-        end,
-        cwd = function()
-	    return vim.fn.getcwd()
-	end,    
-        stopOnEntry = false,
-    },
 }
-'';
-};}
