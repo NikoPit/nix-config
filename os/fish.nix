fb3be3bb4360d65f4893c8e2bcf2d08280626fbe
elysia <3302843873@qq.com>
2026-04-11 21:46:12 +0800

    added hi3rd

--- os/fish.nix
+++ os/fish.nix
@@ -1,3 +1,11 @@
 {
-  programs.fish.enable = true;
+  programs.fish = {
+    enable = true;
+
+    shellInit = ''
+      if test -z "$__NIXOS_SET_ENVIRONMENT_DONE"
+        source /etc/fish/nixos-env-preinit.fish
+      end
+    '';
+  };
 }
