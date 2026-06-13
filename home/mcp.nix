1e83072ddb02f9be06f4a5ee3c3f6dea0467a740
elysia <3302843873@qq.com>
2026-06-13 12:26:47 +0800

    mcp: fix

--- home/mcp.nix
+++ home/mcp.nix
@@ -8,23 +8,18 @@
       fetch.command = "${pkgs.mcp-server-fetch}/bin/mcp-server-fetch";
       context7.command = "${pkgs.context7-mcp}/bin/context7-mcp";
       nixos.command = "${pkgs.mcp-nixos}/bin/mcp-nixos";
+      github.command = "${pkgs.github-mcp-server}/bin/github-mcp-server";
 
       rust-analyzer = {
         command = "${pkgs.mcp-language-server}/bin/mcp-language-server";
         args = [
+          "-workspace"
+          "."
           "-lsp"
           "${pkgs.rust-analyzer}/bin/rust-analyzer"
         ];
       };
 
-      github = {
-        command = "${pkgs.github-mcp-server}/bin/github-mcp-server";
-        args = [
-          "stdio"
-          "--read-only"
-          "--toolsets=repos,issues,pull_requests,git,users"
-        ];
-      };
     };
   };
 }
