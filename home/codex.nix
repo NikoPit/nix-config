9a98ffdb3029c5ec1fc02263d4ddc461eb2eba73
elysia <3302843873@qq.com>
2026-06-13 11:41:55 +0800

    codex: added settings and use e-flowcode

--- home/codex.nix
+++ home/codex.nix
@@ -1,5 +1,14 @@
 {
   programs.codex = {
     enable = true;
+    settings = {
+      model_provider = "e-flowcode";
+      model_providers.e-flowcode = {
+        name = "e-flowcode";
+        base_url = "https://e-flowcode.cc/v1";
+        wire_api = "responces";
+        requires_openai_auth = true;
+      };
+    };
   };
 }
