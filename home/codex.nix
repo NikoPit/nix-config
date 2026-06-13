0235274585cc1588ccbb3b3156353fc34fe52858
elysia <3302843873@qq.com>
2026-06-13 11:51:53 +0800

    codex: fullaccess

--- home/codex.nix
+++ home/codex.nix
@@ -3,6 +3,8 @@
     enable = true;
     settings = {
       model_provider = "e-flowcode";
+      sandbox_mode = "danger-full-access";
+      approval_policy = "never";
       model_providers.e-flowcode = {
         name = "e-flowcode";
         base_url = "https://e-flowcode.cc/v1";
