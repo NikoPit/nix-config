75ee6864dddb759ef7fc5030d72366684abddd99
elysia <3302843873@qq.com>
2026-06-13 11:58:19 +0800

    codex: trust all directories

--- home/codex.nix
+++ home/codex.nix
@@ -5,6 +5,7 @@
       model_provider = "e-flowcode";
       sandbox_mode = "danger-full-access";
       approval_policy = "never";
+      projects."/".trust_level = "trusted";
       model_providers.e-flowcode = {
         name = "e-flowcode";
         base_url = "https://e-flowcode.cc/v1";
