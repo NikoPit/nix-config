d39f4b87c082a33b59d3a24949d3d4e39fda8570
elysia <3302843873@qq.com>
2026-06-22 10:10:18 +0800

    opencode: sets model to gpt5

--- home/opencode.nix
+++ home/opencode.nix
@@ -3,12 +3,17 @@
     enable = true;
     enableMcpIntegration = true;
     settings = {
+      model = "e-flowcode/gpt-5";
+
       provider.e-flowcode = {
         npm = "@ai-sdk/openai";
         name = "e-flowcode";
         options = {
           baseURL = "https://e-flowcode.cc/v1";
           apiKey = "{env:OPENAI_API_KEY}";
+        };
+        models.gpt-5 = {
+          name = "GPT-5";
         };
       };
     };
