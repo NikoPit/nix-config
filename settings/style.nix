8268b47a7b37b4f88cd6ee153c3be6575e16dd1e
elysia <3302843873@qq.com>
2026-07-03 21:45:13 +0800

    style: wallpaper: filled hash

--- settings/style.nix
+++ settings/style.nix
@@ -2,7 +2,7 @@
 {
   wallpaper = pkgs.fetchurl {
     url = "https://wall.alphacoders.com/big.php?i=1266440";
-    hash = "";
+    hash = "sha256-MXbqu0gu0jCOHLMTTKPz9a4O1g1rmoE3x6u5azygoM4=";
   };
   polarity = "light";
 }
