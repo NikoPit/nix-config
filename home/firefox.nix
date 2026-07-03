92cfe7e2db5861080c1dc855e044e232ed939751
elysia <3302843873@qq.com>
2026-07-03 15:23:31 +0800

    firefox: ublock origin plugin

--- home/firefox.nix
+++ home/firefox.nix
@@ -1,6 +1,14 @@
-{ config, ... }: {
+{ config, ... }:
+{
   programs.firefox = {
     enable = true;
     configPath = "${config.xdg.configHome}/mozilla/firefox";
+
+    policies.ExtensionSettings = {
+      "uBlock0@raymondhill.net" = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+        installation_mode = "force_installed";
+      };
+    };
   };
 }
