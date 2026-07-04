18e6a913fab2ba4c4c8a30872a58a22b376c000e
elysia <3302843873@qq.com>
2026-07-04 18:18:08 +0800

    deleted empty systemPackages

--- os/configuration.nix
+++ os/configuration.nix
@@ -38,11 +38,6 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  ];
-
   #programs.firefox.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
