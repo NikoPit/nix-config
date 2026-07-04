f27889dc0eb43f75128e28e19e79fcef98e96754
elysia <3302843873@qq.com>
2026-07-04 11:18:06 +0800

    dunst: unset font and bgcolor (let stylix takeover)

--- home/dunst.nix
+++ home/dunst.nix
@@ -1,21 +1,17 @@
 {
   services.dunst = {
     enable = true;
-    settings = {
-      global = {
-        origin = "bottom-right";
-	font = "JetBrains Mono 11";
-	progress_bar = true;
-	icon_corner_radius = 10;
-	corner_radius = 10;
-	offset = "(0, 100)";
-	width = "(200, 400)";
-	gap_size = 10;
-	corners = "left";
-	frame_width = 0;
 
-	bgcolor = "#2596be80";
-      };
+    settings.global = {
+      origin = "bottom-right";
+      progress_bar = true;
+      icon_corner_radius = 10;
+      corner_radius = 10;
+      offset = "(0, 100)";
+      width = "(200, 400)";
+      gap_size = 10;
+      corners = "left";
+      frame_width = 0;
     };
   };
 }
