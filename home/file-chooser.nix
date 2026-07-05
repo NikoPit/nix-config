f775eb2abd389197a0e5b084d2462ed731250464
elysia <3302843873@qq.com>
2026-07-05 13:49:44 +0800

    deleted pointless file-chooser.nix

--- home/file-chooser.nix
+++
@@ -1,9 +0,0 @@
-{
-  #xdg.configFile."xdg-desktop-portal-termfilechooser/config" = {
-  #  text = ''
-  #    [filechooser]
-  #    cmd=${pkgs.xdg-desktop-portal-termfilechooser}/share/xdg-desktop-portal-termfilechooser/yazi-wrapper.sh
-  #  '';
-  #};
-
-}
