c1a26e0179c6cd4b72f7b873104f62d41e29626c
elysia <3302843873@qq.com>
2026-07-05 13:17:29 +0800

    settings: configPath

--- settings/default.nix
+++ settings/default.nix
@@ -1,8 +1,11 @@
 { pkgs, ... }:
-{
+rec {
   proxy = import ./proxy.nix;
   style = import ./style.nix { inherit pkgs; };
   misc = import ./misc.nix;
   user = import ./user.nix;
   localization = import ./localization.nix;
+
+  # Path to the nix system configuration
+  configPath = /home/${user.name}/nix;
 }
