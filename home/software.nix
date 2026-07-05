26ecb1bd2abf85019ff54862701683e570ef4495
elysia <3302843873@qq.com>
2026-07-05 13:48:14 +0800

    deleted useless software

--- home/software.nix
+++ home/software.nix
@@ -2,34 +2,11 @@
 
 {
   home.packages = with pkgs; [
-    xclicker
-    jdk21
     qq
     gh
     nixfmt
-    osu-lazer-bin
-    go-musicfox
     nodejs
     python3
-    gdb
-    vscodium
-    ripgrep
-    ffmpeg
-    unzip
     killall
-    fd
-    pavucontrol
-    nautilus
-    rustup
-    xray
-    gcc
-    ladybird
-    pkg-config
-    udev
-    libudev-zero
-    gnumake
-    xorriso
-    cpio
-    localsend
   ];
 }
