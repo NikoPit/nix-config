f0bc533a4bf55841dcb5a6913855a6edc0c0763d
elysia <3302843873@qq.com>
2026-07-05 15:47:25 +0800

    fish: removed shellInit

--- modules/fish.nix
+++ modules/fish.nix
@@ -1,11 +1,5 @@
 {
   programs.fish = {
     enable = true;
-
-    shellInit = ''
-      if test -z "$__NIXOS_SET_ENVIRONMENT_DONE"
-        source /etc/fish/nixos-env-preinit.fish
-      end
-    '';
   };
 }
