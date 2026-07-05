ca3163d83a249429bb78af2cec1b102abafdba45
elysia <3302843873@qq.com>
2026-07-05 16:00:39 +0800

    fmt

--- nixvim/keymaps.nix
+++ nixvim/keymaps.nix
@@ -1,40 +1,43 @@
 {
   keymaps = [
     {
-	action = "<cmd>bnext<cr>";
-	key = "<tab>";
-	options = {
-	  silent = true;
-        };	
+      action = "<cmd>bnext<cr>";
+      key = "<tab>";
+      options = {
+        silent = true;
+      };
     }
     {
-        action = "<cmd>Yazi<cr>";
-	key = "<space>-";
-	options.silent = true;
+      action = "<cmd>Yazi<cr>";
+      key = "<space>-";
+      options.silent = true;
     }
     {
-        action = "<cmd>Lspsaga term_toggle<cr>";
-	key = "<A-x>";
-	options.silent = true;
-	mode  = ["n" "t"];
+      action = "<cmd>Lspsaga term_toggle<cr>";
+      key = "<A-x>";
+      options.silent = true;
+      mode = [
+        "n"
+        "t"
+      ];
     }
     {
-        action = "<cmd>LazyGit<cr>";
-	key = "<space>g";
-	options.silent = true;
+      action = "<cmd>LazyGit<cr>";
+      key = "<space>g";
+      options.silent = true;
     }
     {
-    	action = "<cmd>Lspsaga code_action<cr>";
-    	key = "<space>ca";
-	options.silent = true;
+      action = "<cmd>Lspsaga code_action<cr>";
+      key = "<space>ca";
+      options.silent = true;
     }
     {
-    	action = "<cmd>Telescope lsp_definitions<cr>";
-    	key = "gd";
+      action = "<cmd>Telescope lsp_definitions<cr>";
+      key = "gd";
     }
     {
-    	action = "<cmd>Trouble diagnostics toggle<cr>";
-	key = "<space>xx";
+      action = "<cmd>Trouble diagnostics toggle<cr>";
+      key = "<space>xx";
     }
   ];
 }
