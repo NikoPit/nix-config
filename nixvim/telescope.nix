a4c39a38ab80d25fa8f45807fda3cb4c097eb36a
elysia <3302843873@qq.com>
2026-07-05 16:57:19 +0800

    telescope: shortcut for live_grep

--- nixvim/telescope.nix
+++ nixvim/telescope.nix
@@ -6,4 +6,11 @@
 
   # Installs ripgrep for telescope's live_grep
   extraPackages = [ pkgs.ripgrep ];
+
+  keymaps = [
+    {
+      action = "<cmd>Telescope live_grep<cr>";
+      key = "<space>lg";
+    }
+  ];
 }
