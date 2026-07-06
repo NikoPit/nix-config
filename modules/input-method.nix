e550616aa7d69064a7c71e1378e890b23503e8d8
elysia <3302843873@qq.com>
2026-07-06 09:39:51 +0800

    im: fmt
    im: fmt again

--- modules/input-method.nix
+++ modules/input-method.nix
@@ -4,6 +4,7 @@
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
+
     fcitx5 = {
       waylandFrontend = true;
 
@@ -14,11 +15,13 @@
 
       settings.inputMethod = {
         GroupOrder."0" = "Default";
+
         "Groups/0" = {
           Name = "Default";
           "Default Layout" = "us";
           DefaultIM = "pinyin";
         };
+
         "Groups/0/Items/0".Name = "keyboard-us";
         "Groups/0/Items/1".Name = "pinyin";
       };
