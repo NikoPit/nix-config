206dc62168b82ec5d2138d5e5a1e879aa077c89f
elysia <3302843873@qq.com>
2026-07-06 15:29:33 +0800

    deleted pointless mcp

--- modules/mcp.nix
+++ modules/mcp.nix
@@ -3,31 +3,12 @@
 {
   programs.mcp = {
     enable = true;
+
     servers = {
       git.command = "${pkgs.mcp-server-git}/bin/mcp-server-git";
       fetch.command = "${pkgs.mcp-server-fetch}/bin/mcp-server-fetch";
       context7.command = "${pkgs.context7-mcp}/bin/context7-mcp";
       nixos.command = "${pkgs.mcp-nixos}/bin/mcp-nixos";
-      github = {
-        command = "${pkgs.writeShellScript "github-mcp-server-wrapper" ''
-          export GITHUB_PERSONAL_ACCESS_TOKEN="$(${pkgs.gh}/bin/gh auth token)"
-          exec ${pkgs.github-mcp-server}/bin/github-mcp-server "$@"
-        ''}";
-        args = [
-          "stdio"
-        ];
-      };
-
-      rust-analyzer = {
-        command = "${pkgs.mcp-language-server}/bin/mcp-language-server";
-        args = [
-          "-workspace"
-          "."
-          "-lsp"
-          "${pkgs.rust-analyzer}/bin/rust-analyzer"
-        ];
-      };
-
     };
   };
 }
