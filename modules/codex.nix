c2142cc7deead2bd6e64451401e8793a2af7fb4f
elysia <3302843873@qq.com>
2026-07-06 12:09:28 +0800

    codex: fmt

--- modules/codex.nix
+++ modules/codex.nix
@@ -2,17 +2,21 @@
   programs.codex = {
     enable = true;
     enableMcpIntegration = true;
+
     settings = {
       model = "gpt-5.4";
       model_provider = "e-flowcode";
+
       sandbox_mode = "danger-full-access";
       approval_policy = "never";
       projects."/".trust_level = "trusted";
+
       model_providers.e-flowcode = {
         name = "e-flowcode";
         base_url = "https://e-flowcode.cc/v1";
         wire_api = "responses";
         requires_openai_auth = true;
+
         request_max_retries = 100;
         stream_max_retries = 100;
       };
