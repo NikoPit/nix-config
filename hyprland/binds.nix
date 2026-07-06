33ab30b8a39d2e6860cae4676f58ecf49e6edc02
elysia <3302843873@qq.com>
2026-07-06 11:13:57 +0800

    hyprland: binds: switch workspaces

--- hyprland/binds.nix
+++ hyprland/binds.nix
@@ -1,4 +1,14 @@
 [
   "SUPER, Q, exec, ghostty"
   "SUPER, C, killactive"
+
+  "SUPER, 1, workspace, 1"
+  "SUPER, 2, workspace, 2"
+  "SUPER, 3, workspace, 3"
+  "SUPER, 4, workspace, 4"
+  "SUPER, 5, workspace, 5"
+  "SUPER, 6, workspace, 6"
+  "SUPER, 7, workspace, 7"
+  "SUPER, 8, workspace, 8"
+  "SUPER, 9, workspace, 9"
 ]
