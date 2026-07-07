fee04abcd1acf0cdc1263db2bb95bc1be5a42e83
elysia <3302843873@qq.com>
2026-07-07 12:58:13 +0800

    screenshot: takes in wrapper

--- modules/screenshot.nix
+++ modules/screenshot.nix
@@ -4,8 +4,11 @@ let
   grim = "${pkgs.grim}/bin/grim";
   slurp = "${pkgs.slurp}/bin/slurp";
   wl-copy = "${pkgs.wl-clipboard}/bin/wl-copy";
+
   command = "${grim} -g \"$(${slurp})\" - | ${wl-copy}";
+
+  wrapper = pkgs.writeShellScriptBin "screenshot" command;
 in
 {
-  _module.args.screenshot = command;
+  _module.args.screenshot = "${wrapper}/bin/screenshot";
 }
