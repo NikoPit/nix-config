cb02ba980d62e9d04395e3dfe1bd978da624f94b
elysia <3302843873@qq.com>
2026-07-07 21:12:12 +0800

    added nix community cachix
    fix

--- system/misc.nix
+++ system/misc.nix
@@ -7,4 +7,9 @@
   documentation.man.enable = false;
   # Enable wayland for electron apps
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  nix.settings = {
+    substituters = [ "https://nix-community.cachix.org" ];
+    trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+  };
 }
