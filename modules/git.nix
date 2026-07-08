f691f047ea10ae53a7ed9ec01d9b7c5ec9a73f1f
elysia <3302843873@qq.com>
2026-07-08 14:32:33 +0800

    git: uses github token

--- modules/git.nix
+++ modules/git.nix
@@ -1,8 +1,30 @@
-{ settings, ... }:
+{ settings, config, ... }:
+
+let
+  makeCredHelper =
+    { username, password }:
+    "!f() { echo username=${username}; echo password=${password}; }; f";
+
+  githubUsername = settings.user.githubName;
+  githubToken = "$(cat ${config.sops.secrets.github-token.path})";
+
+  githubCredHelper = makeCredHelper {
+    username = githubUsername;
+    password = githubToken;
+  };
+in
 {
+  sops.secrets.github-token = {
+    mode = "0400";
+  };
+
   programs.git = {
     enable = true;
 
-    settings.user = settings.user;
+    settings = {
+      user = settings.user;
+
+      credential."https://github.com".helper = githubCredHelper;
+    };
   };
 }
