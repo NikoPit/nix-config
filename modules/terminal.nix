d536436ca7f5c6d9a82007441f865a93e65b7f9a
elysia <3302843873@qq.com>
2026-07-10 19:16:40 +0800

    terminal: customizes cursor
    terminal: changed cursor opacity

--- modules/terminal.nix
+++ modules/terminal.nix
@@ -1,4 +1,8 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  settings,
+  ...
+}:
 
 let
   ghostty = "${pkgs.ghostty}/bin/ghostty";
@@ -7,6 +11,11 @@ in
 {
   programs.ghostty = {
     enable = true;
+
+    settings = {
+      cursor-color = settings.palette.blue1;
+      cursor-opacity = 0.5;
+    };
   };
 
   wayland.windowManager.hyprland.settings.bind = [ bind ];
