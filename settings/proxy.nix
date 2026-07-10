e2b41fde34a85bea5c4718b63ef2bb92a9376a09
elysia <3302843873@qq.com>
2026-07-10 21:30:10 +0800

    settings: proxy: deleted old url

--- settings/proxy.nix
+++ settings/proxy.nix
@@ -7,6 +7,5 @@
   subscription = {
     updateInterval = 300;
     healthCheckInterval = 300;
-    url = "https://0b96e976-9ec3-44c0-aa2b-30bf8b0792ea.com/sabusuku?token=a4328d9669100b0158e45c0d60fc9724";
   };
 }
