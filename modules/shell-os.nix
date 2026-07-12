4e08382e504cf39db7d4ea4603f7007ed2c2c66c
elysia <3302843873@qq.com>
2026-07-12 18:49:29 +0800

    starship: customize stuff
    starship: customized

--- modules/shell-os.nix
+++ modules/shell-os.nix
@@ -11,6 +11,32 @@
         enable = true;
         left = "starship module character";
       };
+
+      settings = {
+        format = ''
+          [  $directory](bold blue)
+          [ $character](bold darkblue)
+        '';
+        right_format = "$status";
+
+        status = {
+          format = "[$symbol $status]($style)";
+
+          symbol = "";
+          not_found_symbol = "";
+          not_executable_symbol = "󰂭";
+          signal_symbol = "󰉁";
+          sigint_symbol = "󰟾";
+
+          map_symbol = true;
+          pipestatus = true;
+
+          disabled = false;
+        };
+
+        character.format = "[❯ ](bold blue)";
+        add_newline = true;
+      };
     };
   };
 }
