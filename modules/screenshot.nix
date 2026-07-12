bcf3502e0845b6d1c54368a91f8823262d772952
elysia <3302843873@qq.com>
2026-07-12 23:41:22 +0800

    screenshot: save screenshot to Pictures/

--- modules/screenshot.nix
+++ modules/screenshot.nix
@@ -1,11 +1,13 @@
-{ pkgs, ... }:
+{ pkgs, settings, ... }:
 
 let
   grim = "${pkgs.grim}/bin/grim";
   slurp = "${pkgs.slurp}/bin/slurp";
   wl-copy = "${pkgs.wl-clipboard}/bin/wl-copy";
 
-  command = "${grim} -g \"$(${slurp})\" - | ${wl-copy}";
+  screenshotPath = "/home/${settings.user.name}/Pictures/latest_screenshot.png";
+
+  command = "${grim} -g \"$(${slurp})\" - | tee ${screenshotPath} | ${wl-copy}";
 
   wrapper = pkgs.writeShellScriptBin "screenshot" command;
   exec = "${wrapper}/bin/screenshot";
