c50b58ff4c07067c2e5be3e34ed4a499c83ab45d
elysia <3302843873@qq.com>
2026-07-12 15:51:05 +0800

    disable nixos documentations

--- system/misc.nix
+++ system/misc.nix
@@ -4,12 +4,16 @@
   services.getty.autologinUser = settings.user.name;
   security.sudo.wheelNeedsPassword = false;
   programs.command-not-found.enable = false;
-  documentation.man.enable = false;
   # Enable wayland for electron apps
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
   nix.settings = {
     substituters = [ "https://nix-community.cachix.org" ];
     trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+  };
+
+  documentation = {
+    man.enable = false;
+    nixos.enable = false;
   };
 }
