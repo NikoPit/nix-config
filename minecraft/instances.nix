c09f2760c4d9028f9fe127f6bfe264909e73fe3c
elysia <3302843873@qq.com>
2026-07-14 19:28:55 +0800

    minecraft: seperate instance for Fish on MC
    fmt

--- minecraft/instances.nix
+++ minecraft/instances.nix
@@ -1,10 +1,12 @@
 {
   nixcraft.client.instances = {
-    "1.21.11" = {
+    "FishOnMC" = {
       enable = true;
 
       version = "1.21.11";
       assetHash = "sha256-GaqxPrc5xgjlXQDXYyYsES4mYDI0wpvabos7rZHoEXU=";
+
+      desktopEntry.name = "Fish On MC";
     };
   };
 }
