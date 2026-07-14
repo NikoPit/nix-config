b91329a1f8995a4b22cbc01ef8f4d1cdbf91f751
elysia <3302843873@qq.com>
2026-07-14 15:54:38 +0800

    minecraft: instances: deleted 1.21.1 and added 1.21.11

--- minecraft/instances.nix
+++ minecraft/instances.nix
@@ -1,9 +1,9 @@
 {
   nixcraft.client.instances = {
-    "1.21.1" = {
+    "1.21.11" = {
       enable = true;
 
-      version = "1.21.1";
+      version = "1.21.11";
       assetHash = "sha256-GaqxPrc5xgjlXQDXYyYsES4mYDI0wpvabos7rZHoEXU=";
     };
   };
