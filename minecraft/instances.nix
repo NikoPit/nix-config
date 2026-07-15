245978fbc5b13d4e60ec2861faa9682368265aec
elysia <3302843873@qq.com>
2026-07-15 17:45:44 +0800

    minecraft: instances: LiquidBounce (wip)

--- minecraft/instances.nix
+++ minecraft/instances.nix
@@ -8,5 +8,12 @@
 
       desktopEntry.name = "Fish On MC";
     };
+
+    "LiquidBounce" = {
+      enable = true;
+
+      version = "26.1.2";
+      assetHash = "sha256-Jblp3zLP6HOphGT2cWTFvuu+EBkdfdWei9CrGP0xaUc=";
+    };
   };
 }
