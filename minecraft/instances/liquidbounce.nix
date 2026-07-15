0dd00727873192d4ff999e7fcf247369c9216ac8
elysia <3302843873@qq.com>
2026-07-15 21:59:18 +0800

    minecraft: instances: liquidbounce: fixed issues

--- minecraft/instances/liquidbounce.nix
+++ minecraft/instances/liquidbounce.nix
@@ -1,5 +1,15 @@
 { pkgs, ... }:
 
+let
+  jcefSrc = pkgs.fetchFromGitHub {
+    owner = "CCBlueX";
+    repo = "java-cef";
+    rev = "aa20e50dbfb858ea50d3cf405b8202462dd10d96";
+    hash = "sha256-gLDiARy35KixTnS/G8U5NQvm2hjz4yl+O6dgnWNrGMY=";
+  };
+
+  jcef = pkgs.callPackage jcefSrc { };
+in
 {
   enable = true;
 
@@ -20,4 +30,33 @@
       hash = "sha256-NsXdi3KONHDSiCrmMRm5OiBQD8Dqb1yUXBK/ZbWrGDI=";
     };
   };
+
+  runtimeLibs = with pkgs; [
+    libgbm
+    glib
+    nss
+    nspr
+    atk
+    at-spi2-atk
+    libdrm
+    expat
+    libxcb
+    libxkbcommon
+    libXcomposite
+    libXdamage
+    libXext
+    libXfixes
+    libXrandr
+    gtk3
+    pango
+    cairo
+    alsa-lib
+    dbus
+    at-spi2-core
+    cups
+    libxshmfence
+    wayland
+  ];
+
+  envVars.PROVIDED_JCEF_PATH = "${jcef}";
 }
