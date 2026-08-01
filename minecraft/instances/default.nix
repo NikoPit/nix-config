e1f5aee3e1a7fc36d3cd4ae2f85abaa73dcc40cf
elysia <3302843873@qq.com>
2026-08-01 20:32:09 +0800

    minecraft: deleted harryspit and fishonmc

--- minecraft/instances/default.nix
+++ minecraft/instances/default.nix
@@ -2,24 +2,6 @@
 
 {
   nixcraft.client.instances = {
-    "FishOnMC" = {
-      enable = true;
-
-      version = "1.21.11";
-      assetHash = "sha256-GaqxPrc5xgjlXQDXYyYsES4mYDI0wpvabos7rZHoEXU=";
-
-      desktopEntry.name = "Fish On MC";
-    };
-
-    "HarrysPit" = {
-      enable = true;
-
-      version = "1.8.9";
-      assetHash = "sha256-aZu7DkjlpUqITM9qMeZ8gVYzNFkmm1UhiorevYUGxwg=";
-
-      desktopEntry.name = "Harrys Pit";
-    };
-
     "LiquidBounce" = import ./liquidbounce.nix { inherit pkgs; };
     "Skyblock" = import ./skyblock.nix { inherit pkgs; };
     "Wynncraft" = import ./wynncraft.nix { inherit pkgs; };
