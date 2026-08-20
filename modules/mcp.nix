8d525f58b451c81ab409f30d77816f61c7015607
elysia <3302843873@qq.com>
2026-08-20 13:19:05 +0800

    mcp: add playwright

--- modules/mcp.nix
+++ modules/mcp.nix
@@ -9,6 +9,7 @@
       fetch.command = "${pkgs.mcp-server-fetch}/bin/mcp-server-fetch";
       context7.command = "${pkgs.context7-mcp}/bin/context7-mcp";
       nixos.command = "${pkgs.mcp-nixos}/bin/mcp-nixos";
+      playwright.command = "${pkgs.playwright-mcp}/bin/playwright-mcp";
     };
   };
 }
