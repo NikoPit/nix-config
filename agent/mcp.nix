f1ba58056927d437dac6283cc59d44012cfb3f4c
elysia <3302843873@qq.com>
2026-08-21 20:48:19 +0800

    agent: mcp: removed fetch

--- agent/mcp.nix
+++ agent/mcp.nix
@@ -6,7 +6,6 @@
 
     servers = {
       git.command = "${pkgs.mcp-server-git}/bin/mcp-server-git";
-      fetch.command = "${pkgs.mcp-server-fetch}/bin/mcp-server-fetch";
       context7.command = "${pkgs.context7-mcp}/bin/context7-mcp";
       nixos.command = "${pkgs.mcp-nixos}/bin/mcp-nixos";
       playwright.command = "${pkgs.playwright-mcp}/bin/playwright-mcp";
