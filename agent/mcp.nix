f9aa72bcbe564e8c49a9827da0162651ddfa851e
elysia <3302843873@qq.com>
2026-08-21 20:48:36 +0800

    agent: mcp: removed git

--- agent/mcp.nix
+++ agent/mcp.nix
@@ -5,7 +5,6 @@
     enable = true;
 
     servers = {
-      git.command = "${pkgs.mcp-server-git}/bin/mcp-server-git";
       context7.command = "${pkgs.context7-mcp}/bin/context7-mcp";
       nixos.command = "${pkgs.mcp-nixos}/bin/mcp-nixos";
       playwright.command = "${pkgs.playwright-mcp}/bin/playwright-mcp";
