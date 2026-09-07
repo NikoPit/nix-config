8a367bfa02111bac7566192be976ad5074edd178
elysia <3302843873@qq.com>
2026-09-07 20:32:11 +0800

    minecraft: mute gameplay music and jukebox sounds

--- linux/minecraft/options.nix
+++ linux/minecraft/options.nix
@@ -5,4 +5,9 @@
   toggleSprint = true;
   gamma = 1.0;
   guiScale = 2;
+
+  # Mute gameplay music and jukebox/note-block sounds (options.txt keys
+  # soundCategory_music / soundCategory_record). Floats render as 0.000000.
+  "soundCategory_music" = 0.0;
+  "soundCategory_record" = 0.0;
 }
