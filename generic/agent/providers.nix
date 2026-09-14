292c5f63fe3645bb330e665185156344bd895e55
elysia <3302843873@qq.com>
2026-09-14 13:54:26 +0800

    agent: providers: e-flowcode-cn: use anthropic-messages api

--- generic/agent/providers.nix
+++ generic/agent/providers.nix
@@ -92,8 +92,8 @@
 
     e-flowcode-cn = {
       headers.User-Agent = "codex_cli_rs/0.77.0 (Windows 10.0.26100; x86_64) WindowsTerminal";
-      baseUrl = "https://e-flowcode.cc/v1";
-      api = "openai-completions";
+      baseUrl = "https://e-flowcode.cc";
+      api = "anthropic-messages";
       apiKey = "!cat ${config.sops.secrets.e-flowcode-cn-apikey.path}";
 
       models = [
