0ba301c67b80dfcb325bb0ae78be382815f762df
elysia <3302843873@qq.com>
2026-09-20 10:31:14 +0800

    linux: ssh: enable mosh

--- linux/ssh.nix
+++ linux/ssh.nix
@@ -13,6 +13,11 @@
     };
   };
 
+  programs.mosh = {
+    enable = true;
+    openFirewall = true;
+  };
+
   users.users.${settings.user.name}.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDGKNnGONe4UkyPU+3rcM1JWd7VdozYiKBjbhn1gKYEN"
   ];
