e4b5a907fecf3d5fb24257287e1a19c81b828c29
elysia <3302843873@qq.com>
2026-09-20 10:27:20 +0800

    nixvim: c: add cpp using clang-format cuz .h is detected as cpp

--- generic/nixvim/lang/c.nix
+++ generic/nixvim/lang/c.nix
@@ -6,7 +6,12 @@
       config.plugins.treesitter.package.builtGrammars.c
     ];
 
-    conform-nvim.settings.formatters_by_ft.c = [ "clang-format" ];
+    conform-nvim.settings.formatters_by_ft = {
+      c = [ "clang-format" ];
+
+      # .h files are detected as cpp
+      cpp = [ "clang-format" ];
+    };
 
     lsp.servers.clangd.enable = true;
   };
